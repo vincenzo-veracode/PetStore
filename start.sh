3f98ab65fa4fe8e6d5063f54a6bb0c2e8b91eb30
Kevin Rise <krise@veracode.com>
2021-11-16 07:23:23 -0500

    full start script

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#/etc/init.d/mariadb start &
-#sleep 5
-#mysql -e "CREATE DATABASE flask_api;"
-#mysql -e "CREATE USER 'petstore'@'%' IDENTIFIED BY 'Ver@c0de';"
-#mysql -e "GRANT ALL ON *.* TO 'petstore'@'%';"
-#mysql -e "flush privileges;"
+/etc/init.d/mysql start &
+sleep 5
+mysql -e "CREATE DATABASE flask_api;"
+mysql -e "CREATE USER 'petstore'@'%' IDENTIFIED BY 'Ver@c0de';"
+mysql -e "GRANT ALL ON *.* TO 'petstore'@'%';"
+mysql -e "flush privileges;"
 
 python3 api.py
